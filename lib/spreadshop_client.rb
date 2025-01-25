7cbc2c184aaf6d489805f110fdf3002c5bd80bbb
Paderich <paderich@protonmail.com>
2025-01-25 12:52:41 +0100

    $ bundle gem spreadshop_client

---
+++ lib/spreadshop_client.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "spreadshop_client/version"
+
+module SpreadshopClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_spreadshop_client.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestSpreadshopClient < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::SpreadshopClient::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
