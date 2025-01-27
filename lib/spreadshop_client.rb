a4581eac817ac703218e0562a469f0f213069e6b
Paderich <paderich@protonmail.com>
2025-01-27 08:01:07 +0100

    refactor Product, to also get product by id

--- lib/spreadshop_client.rb
+++ lib/spreadshop_client.rb
@@ -16,4 +16,8 @@ module SpreadshopClient
   def self.get_all(shop_id:, platform:, locale:)
     Product.get_all(shop_id: shop_id, platform: platform, locale: locale)
   end
+
+  def self.get_product_by_id(product_id:, shop_id:, platform:, locale:)
+    Product.get_by_id(product_id: product_id, shop_id: shop_id, platform: platform, locale: locale)
+  end
 end
